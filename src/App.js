d8ad8c8ca4ebb4d10b02dc4966bc1e94a41916ca
Alex Korjeski <akorjeski@gmail.com>
2021-03-25 16:51:19 -0400

    added an input field. the value there works in the deathroll function when you click it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
+
+const [playerRoll, setPlayerRoll] = useState();
+function deathroll(){console.log(Math.floor(Math.random() * playerRoll) + 1)}
   return (
     <div className="App">
       <header>
-        <img src={logo} className="App-logo" alt="logo" />
+        <input
+        onChange={e => setPlayerRoll(e.target.value)}></input>
+        {playerRoll}
+        <button onClick={deathroll}>Click me to deathroll!</button>
         <h1>We now have Auth!</h1>
       </header>
       <AmplifySignOut />

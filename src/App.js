cce5f29f3a6956bc952da49afd2a2a144d4f0fe0
Alex Korjeski <akorjeski@gmail.com>
2021-03-25 17:24:31 -0400

    enter a value and click. values are added to an array and displayed. this is to show the rolls each player has until the game ends

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
 
-const [playerRoll, setPlayerRoll] = useState();
-function deathroll(){console.log(Math.floor(Math.random() * playerRoll) + 1)}
+  const [wager, setWager] = useState();
+  const [player,setPlayer] = useState([]);
+  const [gorp,setGorp] = useState([]);
+
+  function deathroll() { 
+    const startingRoll = wager * 2;
+    setPlayer([...player,((Math.floor(Math.random() * startingRoll) + 1))]) }
+
   return (
     <div className="App">
       <header>
-        <input
-        onChange={e => setPlayerRoll(e.target.value)}></input>
-        {playerRoll}
+        <input onChange={e => setWager(e.target.value)}></input>
+        {player}
         <button onClick={deathroll}>Click me to deathroll!</button>
-        <h1>We now have Auth!</h1>
       </header>
-      <AmplifySignOut />
+      {/* <AmplifySignOut /> */}
     </div>
   );
 }
